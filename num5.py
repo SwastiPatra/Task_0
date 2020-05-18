c0ac9b9014d9dc69cc1c4b6cbac60bf70dc02f76
SwastiPatra <64317533+SwastiPatra@users.noreply.github.com>
2020-05-18 09:21:58 +0530

    Update num5.py

--- num5.py
+++ num5.py
@@ -1,4 +1,23 @@
-set1 = {1,2,3,4,5,6,7,8,9}
-set2 = {1,3,4,6,8,11,22,34,51,67}
-set1.difference_update(set2)
-print(set1)
+
+#program to print all the prime numbers in a given interval
+lower = int(input("Enter the lower range: "))
+upper = int(input("Enter the upper range: "))
+for n in range(lower,upper+1):
+    if(n > 1):
+        for i in range(2,n):
+          if(n % i) == 0:
+              break
+        else:
+          print(n)
+#now to check a given number is prime or not
+a= int(input("Enter a number: "))
+j=1
+if(a > 1):
+  for j in range(2,a):
+      if(a % j) == 0:
+          print(a,"is not a prime number")
+          break
+  else:
+          print(a,"is prime")
+else:
+      print(a,"is not a prime number")   

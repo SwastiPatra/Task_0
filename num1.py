d980597ecde19114c853145f09dc006ca5409858
SwastiPatra <64317533+SwastiPatra@users.noreply.github.com>
2020-05-18 08:52:03 +0530

    Update num1.py

--- num1.py
+++ num1.py
@@ -1,10 +1,10 @@
-def outerFun(a,b):
-     square = a**2
-     def innerFun(a,b):
-         return a+b
-     add = innerFun(a,b)
-     return add+5
-a = int(input("enter value of a: " ))
-b = int(input("enter value of b: "))
-result = outerFun(a,b)
-print(result)
+def multiplication_or_sum(a,b):
+    product = a*b
+    if(product<=1000):
+      return product
+    else:
+      return a+b
+a= int(input("enter first number: "))
+b= int(input("enter second number: "))
+result=multiplication_or_sum(a,b)
+print("The result is",result)

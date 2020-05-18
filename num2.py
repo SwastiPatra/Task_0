30ec0dcafa685a5a2912da65fca187f5ee472d93
SwastiPatra <64317533+SwastiPatra@users.noreply.github.com>
2020-05-18 09:16:28 +0530

    Update num2.py

--- num2.py
+++ num2.py
@@ -1,12 +1,5 @@
-Str = input("enter a string: ")
-words = Str.split()
-lower = []
-upper = []
-for char in Str:
-    if char.islower():
-        lower.append(char)
-    else:
-        upper.append(char)
-sortedstring = ''.join(lower + upper)
-print("\n arranging characters giving precedence to lowercase:")
-print(sortedstring)
+num=int(input("enter the value of n: "))
+sum=0
+for num in range(0,num+1,1):
+       sum= sum+num; 
+print("SUM = ", sum)
